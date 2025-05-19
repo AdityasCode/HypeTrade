9e293a9201b7df514f6e7d2b2e0c42ec07c4998c
Aditya Gandhi <adityagandhi98101@gmail.com>
2025-05-19 11:03:43 +0530

    Added an Error Boundary to the React app to prevent total shutdown of any page because of error(/s).

--- HypeTrade307/src/main.tsx
+++ HypeTrade307/src/main.tsx
@@ -1,11 +1,13 @@
-//@ts-nocheck
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css"
 import App from './App.tsx'
+import { MyAppErrorBoundary } from 'src/components/MyAppErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <MyAppErrorBoundary>
+            <App />
+        </MyAppErrorBoundary>
+    </StrictMode>,
 )
