7964be5c8558f90b3396452bf044532a47ba9ae4
Aditya Gandhi <adityagandhi98101@gmail.com>
2025-09-04 16:08:20 -0400

    Fixing API URL, updating deprecated frontend packages.

--- HypeTrade307/src/config.ts
+++ HypeTrade307/src/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://hypetrade-gcp-server-800055345422.us-central1.run.app';
+export const API_BASE_URL = 'https://hypetrade-gcp-server-800055345422.us-central1.run.app/api';
 // export const API_BASE_URL = 'https://hypetrade-800055345422.us-central1.run.app/api';
 // export const API_BASE_URL = '/api';
 // export const API_BASE_URL = 'https://hypet-aditya-145797464141.us-central1.run.app/api';
